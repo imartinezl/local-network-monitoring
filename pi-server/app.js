973ffac5604d3fa3d47033c463b7319f8d556003
imartinezl <inigomlap@gmail.com>
2019-08-17 11:41:49 +0200

    Plotly plot in plain JS

--- pi-server/app.js
+++ pi-server/app.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
 
+
 app.get('/', function(req, res){
-    res.send('Hello World!');
+    res.sendFile(__dirname + '/index.html');
 });
+
+app.use(express.static('public'));
 
 app.listen(3000, function(){
     console.log('Listening on port 3000!');
