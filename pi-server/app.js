0cd6ebfd610a8b5e0195da2755666afbed538e2a
imartinezl <inigomlap@gmail.com>
2019-08-19 21:43:27 +0200

    Served static files

--- pi-server/app.js
+++ pi-server/app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+app.use(express.static('../'));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
-
-app.use(express.static('public'));
 
 app.listen(3000, function(){
     console.log('Listening on port 3000!');
